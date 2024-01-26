a594eb6c41d04a304d6cc617870b6d80f09a921c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-01-26 21:16:45 +0200

    setup: package

--- prebuild.js
+++ prebuild.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 
 
 const fetchInterviews = async () => {
+    const dataFile = './app/interviews/data.json';
+
     const link = process.env.INTERVIEWS_URL;
     if (!link) {
+        const emptyData = {
+            interviews: [],
+        };
+        await fs.promises.writeFile(dataFile, JSON.stringify(emptyData));
         return;
     }
 
-    const dataFile = './app/interviews/data.json';
     const response = await fetch(link);
     const json = await response.json();
 
