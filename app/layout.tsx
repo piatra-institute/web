4158f1d4bda4fab1272e62db48f8485ac2ae84a1
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-01-27 15:00:56 +0200

    dev: app

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
     children: React.ReactNode,
 }) {
     return (
-        <html lang="en">
+        <html
+            lang="en"
+            suppressHydrationWarning
+        >
             <body
                 className={`${fonts}`}
             >
