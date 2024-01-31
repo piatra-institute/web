a55f9b7930350b250845b9c2b9a0afafe6a9f997
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-01-31 19:06:43 +0200

    dev: app, public

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import type { Viewport } from 'next';
 import type { Metadata } from 'next';
 
-import './globals.css';
-
 import fonts from '@/data/fonts';
+
+import './globals.css';
 
 
 
@@ -12,7 +13,42 @@ export const metadata: Metadata = {
         template: '%s · piatra.institute',
     },
     description: 'love · care · deep research',
+    creator: 'piatra.institute',
+    publisher: 'piatra.institute',
+    robots: 'index, follow',
+    referrer: 'origin',
+    metadataBase: new URL('https://piatra.institute'),
+    alternates: {
+        canonical: 'https://piatra.institute',
+    },
+    icons: {
+        other: [
+            {
+                rel: 'mask-icon',
+                url: '/safari-pinned-tab.svg',
+                color: '#5bbad5',
+            },
+        ],
+    },
+    openGraph: {
+        type: 'website',
+        url: 'https://piatra.institute',
+        title: 'piatra.institute',
+        description: 'love · care · deep research',
+        siteName: 'piatra.institute',
+        images: [
+            {
+                url: 'https://piatra.institute/og.png',
+            },
+        ],
+    },
 };
+
+
+export const viewport: Viewport = {
+    themeColor: '#000000',
+};
+
 
 export default function RootLayout({
     children,
