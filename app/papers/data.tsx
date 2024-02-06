c9b7833bcac324936e88db65a2cdbfe0c715e1f7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-02-06 18:22:13 +0200

    dev: app, data

---
+++ app/papers/data.tsx
@@ -0,0 +1,9 @@
+import data from './data.json';
+
+import {
+    PapersData,
+} from '@/data';
+
+
+
+export default data as PapersData;
--- data/index.ts
+++ data/index.ts
@@ -1,9 +1,32 @@
-export interface Concern {
+export interface Paper {
     id: string;
-    text: string;
-    references: string[];
-    context: string;
+    path: string;
+    date: string;
+    title: string;
+    abstract: string;
+    metadata: {
+        title: string;
+        description: string;
+    };
+    text: PaperText;
 }
+
+export interface PaperText {
+    sections: PaperSection[];
+}
+
+export interface PaperSection {
+    id: string;
+    title: string;
+    paragraphs: string[];
+}
+
+
+export interface PapersData {
+    papers: Paper[];
+}
+
+
 
 export interface Provocation {
     id: string;
@@ -17,6 +40,14 @@ export interface Provocation {
     };
     concerns: Concern[];
 }
+
+export interface Concern {
+    id: string;
+    text: string;
+    references: string[];
+    context: string;
+}
+
 
 export interface ProvocationsData {
     provocations: Provocation[];
