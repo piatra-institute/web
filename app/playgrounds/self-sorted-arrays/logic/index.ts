72d2e7da8b50d0c51a1fede56668198aba4e0c88
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-02-10 13:29:45 +0200

    dev: playgrounds/self-sorted-arrays

--- app/playgrounds/self-sorted-arrays/logic/index.ts
+++ app/playgrounds/self-sorted-arrays/logic/index.ts
@@ -24,20 +24,22 @@ export const bubbleAlgotype = (
 
 
 
-export const biasedRandomColor = () => `rgb(${10 + Math.random() * 30}, ${10 + Math.random() * 50}, ${80 + Math.random() * 155})`;
+export const biasedRandomColor = () => (
+    `rgb(${10 + Math.random() * 30}, ${10 + Math.random() * 50}, ${80 + Math.random() * 155})`
+);
 export const biasedBlueRandomColor = (value: number) => {
-    const invertedValue = 100 - value;
-    const baseRed = 10 + invertedValue * 0.3;
-    const baseGreen = 10 + invertedValue * 0.5;
-    const baseBlue = 80 + invertedValue * 1.55;
+    const invertedValue = 100 - value + (Math.random() * value * 0.5);
+    const baseRed = Math.floor(10 + invertedValue * 0.3);
+    const baseGreen = Math.floor(10 + invertedValue * 0.5);
+    const baseBlue = Math.floor(80 + invertedValue * 1.55);
 
     return `rgb(${baseRed}, ${baseGreen}, ${baseBlue})`;
 };
 export const biasedLimeRandomColor = (value: number) => {
-    const invertedValue = 100 - value;
-    const baseRed = 140 + invertedValue * 0.8;
-    const baseGreen = 170 + invertedValue * 0.4;
-    const baseBlue = 140 + invertedValue * 0.3;
+    const invertedValue = 100 - value + (Math.random() * value * 0.5);
+    const baseRed = Math.floor(140 + invertedValue * 0.8);
+    const baseGreen = Math.floor(170 + invertedValue * 0.4);
+    const baseBlue = Math.floor(140 + invertedValue * 0.3);
 
     return `rgb(${baseRed}, ${baseGreen}, ${baseBlue})`;
 };
