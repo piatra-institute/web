fd525cb82f2b243eafaaf81827e370ca35e6e2ab
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-02-26 21:48:12 +0200

    dev: self-sorted-arrays

--- app/playgrounds/self-sorted-arrays/logic/__tests__/entities.test.ts
+++ app/playgrounds/self-sorted-arrays/logic/__tests__/entities.test.ts
@@ -47,3 +47,89 @@ test('Tissue sorts bubble', () => {
 
     expect(tissue.atEquilibrium).toBe(true);
 });
+
+
+
+test('Tissue sorts bubble mutationable', () => {
+    const tissue = new Tissue();
+
+    const cells = [
+        [
+            'cell6',
+            6,
+            'red',
+            'bubble',
+        ],
+        [
+            'cell4',
+            4,
+            'blue',
+            'bubble',
+        ],
+        [
+            'cell3',
+            3,
+            'green',
+            'bubble',
+        ],
+        [
+            'cell1',
+            1,
+            'green',
+            'bubble',
+        ],
+        [
+            'cell1b',
+            1,
+            'green',
+            'bubble',
+        ],
+        [
+            'cell1c',
+            1,
+            'green',
+            'bubble',
+        ],
+        [
+            'cell5',
+            5,
+            'green',
+            'bubble',
+        ],
+    ] as CellsType;
+
+    for (const cell of cells) {
+        tissue.addCell(new Cell(
+            ...cell,
+            'proactive',
+            undefined,
+            3,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            undefined,
+            {
+                damageablePassiveThreshold: 2,
+                damageableFrozenThreshold: 3,
+            },
+        ));
+    }
+
+    tissue.step();
+    // console.log(tissue.cells);
+    tissue.step();
+    // console.log(tissue.cells);
+    tissue.step();
+    // console.log(tissue.cells);
+    tissue.step();
+    // console.log(tissue.cells);
+    tissue.step();
+    // console.log(tissue.cells);
+    tissue.step();
+    // console.log(tissue.cells);
+    tissue.step();
+    // console.log(tissue.cells);
+
+    // expect(tissue.atEquilibrium).toBe(true);
+});
