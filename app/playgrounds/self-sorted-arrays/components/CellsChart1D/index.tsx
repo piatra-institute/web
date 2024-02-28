3bc1e90f81ce481154e3ec0b39e0bf4fcfb4d962
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-02-28 20:18:24 +0200

    dev: self-sorted-arrays

--- app/playgrounds/self-sorted-arrays/components/CellsChart1D/index.tsx
+++ app/playgrounds/self-sorted-arrays/components/CellsChart1D/index.tsx
@@ -2,13 +2,22 @@
 
 import {
     useRef,
+    useState,
+    useMemo,
     useEffect,
 } from 'react';
 
 import {
-    Cell,
+    CellData,
 } from '@/app/playgrounds/self-sorted-arrays/data';
 
+
+
+const computeCanvasHeight = (
+    width: number,
+) => {
+    return Math.floor(width * 2/3);
+}
 
 
 const CanvasColumnChart = ({
@@ -16,131 +25,29 @@ const CanvasColumnChart = ({
     selectedCell,
     setSelectedCell,
 }: {
-    data: Cell[];
+    data: CellData[];
     selectedCell: string | null;
     setSelectedCell: React.Dispatch<React.SetStateAction<string | null>>;
 }) => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    const canvasWidth = 550;
-    const canvasHeight = 350;
-    const columnWidth = canvasWidth / data.length;
-    const maxDataValue = Math.max(
-        ...data.map((cell) => Math.abs(cell.value))
-    );
 
-    useEffect(() => {
-        const canvas = canvasRef.current!;
-        const ctx = canvas.getContext('2d')!;
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    const [canvasWidth, setCanvasWidth] = useState(550);
+    const [canvasHeight, setCanvasHeight] = useState(computeCanvasHeight(canvasWidth));
 
-        let selectedColumnHeight = 0;
-        let selectedColumnIndex = 0;
 
-        const topPadding = 22;
+    const columnWidth = useMemo(() => canvasWidth / data.length, [
+        canvasWidth,
+        data.length,
+    ]);
+    const maxDataValue = useMemo(() => (
+        Math.max(
+            ...data.map((cell) => Math.abs(cell.value))
+        )
+    ), [
+        data,
+    ]);
 
-        const computeDimensions = (
-            cell: Cell,
-            index: number,
-        ) => {
-            const columnHeight = (Math.abs(cell.value) / maxDataValue) * canvasHeight;
-            if (cell.id === selectedCell) {
-                selectedColumnHeight = columnHeight;
-                selectedColumnIndex = index;
-            }
-            const x = index * columnWidth;
-            const y = canvasHeight - columnHeight + topPadding;
-
-            return {
-                x,
-                y,
-                columnHeight,
-            };
-        }
-
-        data.forEach((cell, index) => {
-            const {
-                columnHeight,
-                x,
-                y,
-            } = computeDimensions(cell, index);
-            if (cell.id === selectedCell) {
-                selectedColumnHeight = columnHeight;
-                selectedColumnIndex = index;
-            }
-
-            ctx.fillStyle = cell.color;
-            ctx.fillRect(x, y, columnWidth, columnHeight);
-
-            if (cell.value < 0) {
-                const dotRadius = 2;
-
-                const numDotsX = Math.floor(columnWidth / (dotRadius * 2));
-                const numDotsY = Math.floor(columnHeight / (dotRadius * 2));
-
-                const dotSpacingX = columnWidth / (numDotsX + 1);
-                const dotSpacingY = columnHeight / (numDotsY + 1);
-
-                for (let i = 1; i <= numDotsX; i++) {
-                    if (i % 2 === 0) {
-                        continue;
-                    }
-
-                    for (let j = 1; j <= numDotsY; j++) {
-                        if (j % 2 === 0) {
-                            continue;
-                        }
-
-                        const dotX = x + i * dotSpacingX;
-                        const dotY = y + j * dotSpacingY;
-                        ctx.beginPath();
-                        ctx.arc(dotX, dotY, dotRadius, 0, Math.PI * 2);
-                        ctx.fillStyle = 'black';
-                        ctx.fill();
-                        ctx.closePath();
-                    }
-                }
-            }
-
-            if (data.length < 45) {
-                ctx.fillStyle = 'white';
-                ctx.font = '9px Arial';
-                ctx.textAlign = 'center';
-                ctx.fillText(cell.value.toString(), x + columnWidth / 2, canvasHeight - 5);
-            }
-        });
-
-        data.forEach((cell, index) => {
-            const {
-                columnHeight,
-                x,
-                y,
-            } = computeDimensions(cell, index);
-
-            if (cell.swap && cell.swap !== 'proactive') {
-                ctx.fillStyle = 'white';
-                ctx.font = '13px Arial';
-                ctx.textAlign = 'center';
-                const cellSwap = cell.swap === 'frozen' ? '❄️' : '⚪';
-                const topElevation = columnHeight < 20
-                    ? y + 28
-                    : y - 6;
-                ctx.fillText(cellSwap, x + columnWidth / 2, topElevation);
-            }
-        });
-
-        if (selectedCell !== null) {
-            ctx.strokeStyle = 'white';
-            const lineWidth = 2;
-            ctx.lineWidth = lineWidth;
-            const selectedX = selectedColumnIndex * columnWidth;
-            const selectedY = canvasHeight - lineWidth / 2 - 4;
-            ctx.strokeRect(
-                selectedX + 2, selectedY + 4,
-                columnWidth - 4, -selectedColumnHeight + topPadding + 2,
-            );
-        }
-    }, [data, maxDataValue, canvasWidth, canvasHeight, columnWidth, selectedCell]);
 
     const handleColumnClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
         event.preventDefault();
@@ -162,6 +69,146 @@ const CanvasColumnChart = ({
             setSelectedCell(cell.id);
         }
     };
+
+
+    useEffect(() => {
+        const draw = () => {
+            const canvas = canvasRef.current!;
+            const ctx = canvas.getContext('2d')!;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+            let selectedColumnHeight = 0;
+            let selectedColumnIndex = 0;
+
+            const topPadding = 22;
+
+            const computeDimensions = (
+                cell: CellData,
+                index: number,
+            ) => {
+                const columnHeight = (Math.abs(cell.value) / maxDataValue) * canvasHeight;
+                if (cell.id === selectedCell) {
+                    selectedColumnHeight = columnHeight;
+                    selectedColumnIndex = index;
+                }
+                const x = index * columnWidth;
+                const y = canvasHeight - columnHeight + topPadding;
+
+                return {
+                    x,
+                    y,
+                    columnHeight,
+                };
+            }
+
+            data.forEach((cell, index) => {
+                const {
+                    columnHeight,
+                    x,
+                    y,
+                } = computeDimensions(cell, index);
+                if (cell.id === selectedCell) {
+                    selectedColumnHeight = columnHeight;
+                    selectedColumnIndex = index;
+                }
+
+                ctx.fillStyle = cell.color;
+                ctx.fillRect(x, y, columnWidth, columnHeight);
+
+                if (cell.value < 0) {
+                    const dotRadius = 2;
+
+                    const numDotsX = Math.floor(columnWidth / (dotRadius * 2));
+                    const numDotsY = Math.floor(columnHeight / (dotRadius * 2));
+
+                    const dotSpacingX = columnWidth / (numDotsX + 1);
+                    const dotSpacingY = columnHeight / (numDotsY + 1);
+
+                    for (let i = 1; i <= numDotsX; i++) {
+                        if (i % 2 === 0) {
+                            continue;
+                        }
+
+                        for (let j = 1; j <= numDotsY; j++) {
+                            if (j % 2 === 0) {
+                                continue;
+                            }
+
+                            const dotX = x + i * dotSpacingX;
+                            const dotY = y + j * dotSpacingY;
+                            ctx.beginPath();
+                            ctx.arc(dotX, dotY, dotRadius, 0, Math.PI * 2);
+                            ctx.fillStyle = 'black';
+                            ctx.fill();
+                            ctx.closePath();
+                        }
+                    }
+                }
+
+                if (data.length < 45) {
+                    ctx.fillStyle = 'white';
+                    ctx.font = '9px Arial';
+                    ctx.textAlign = 'center';
+                    ctx.fillText(cell.value.toString(), x + columnWidth / 2, canvasHeight - 5);
+                }
+            });
+
+            data.forEach((cell, index) => {
+                const {
+                    columnHeight,
+                    x,
+                    y,
+                } = computeDimensions(cell, index);
+
+                if (cell.swap && cell.swap !== 'proactive') {
+                    ctx.fillStyle = 'white';
+                    ctx.font = '13px Arial';
+                    ctx.textAlign = 'center';
+                    const cellSwap = cell.swap === 'frozen' ? '❄️' : '⚪';
+                    const topElevation = columnHeight < 20
+                        ? y + 28
+                        : y - 6;
+                    ctx.fillText(cellSwap, x + columnWidth / 2, topElevation);
+                }
+            });
+
+            if (selectedCell !== null) {
+                ctx.strokeStyle = 'white';
+                const lineWidth = 2;
+                ctx.lineWidth = lineWidth;
+                const selectedX = selectedColumnIndex * columnWidth;
+                const selectedY = canvasHeight - lineWidth / 2 - 4;
+                ctx.strokeRect(
+                    selectedX + 2, selectedY + 4,
+                    columnWidth - 4, -selectedColumnHeight + topPadding + 2,
+                );
+            }
+        }
+
+        draw();
+
+        const resizeCanvas = () => {
+            const width = window.innerWidth < 600 ? 300 : 550;
+            const height = computeCanvasHeight(width);
+
+            setCanvasWidth(width);
+            setCanvasHeight(height);
+            draw();
+        };
+
+        resizeCanvas();
+        window.addEventListener('resize', resizeCanvas);
+
+        return () => window.removeEventListener('resize', resizeCanvas);
+    }, [
+        data,
+        maxDataValue,
+        canvasWidth,
+        canvasHeight,
+        columnWidth,
+        selectedCell,
+    ]);
+
 
     return (
         <canvas
