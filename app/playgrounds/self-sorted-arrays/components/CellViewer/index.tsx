abac591fa4b82aa18faa20cec1f8f401b8e20cbd
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-02-29 21:01:27 +0200

    dev: self-sorted-arrays

--- app/playgrounds/self-sorted-arrays/components/CellViewer/index.tsx
+++ app/playgrounds/self-sorted-arrays/components/CellViewer/index.tsx
@@ -11,7 +11,7 @@ import {
 } from '@/data/styles';
 
 import {
-    Cell,
+    CellData,
 } from '@/app/playgrounds/self-sorted-arrays/data';
 
 import {
@@ -21,8 +21,8 @@ import {
 
 
 export interface CellViewerProps {
-    data: Cell;
-    distribution: Cell[];
+    data: CellData;
+    distribution: CellData[];
     swaps: string[];
     close: () => void;
 }
@@ -57,7 +57,7 @@ const CellViewer: React.FC<CellViewerProps> = ({
 
     return (
         <div
-            className="fixed z-30 top-0 left-0 md:left-auto right-0 bottom-0 w-full md:w-[550px] flex flex-col items-center justify-center  overflow-scroll"
+            className="fixed z-30 top-0 left-0 md:left-auto right-0 bottom-0 w-full md:w-[550px] flex flex-col overflow-scroll"
             style={{
                 backgroundColor: data.color.replace('rgb', 'rgba').replace(')', ', 0.5)'),
             }}
@@ -71,49 +71,53 @@ const CellViewer: React.FC<CellViewerProps> = ({
                 {closeIcon}
             </button>
 
-            <h2
-                className="text-3xl font-bold text-white text-center m-4 select-none"
+            <div
+                className="m-auto"
             >
-                cell &lt;{data.value}&gt;
-            </h2>
-
-            <h3
-                className="text-xl font-bold text-white text-center m-4 select-none"
-            >
-                &lsquo;{data.algotype}&rsquo; algotype · &lsquo;{data.swap || 'proactive'}&rsquo; swap
-            </h3>
-
-            {swaps.length > 0 && (
-                <div
-                    className="flex flex-col items-center justify-center text-white m-4"
+                <h2
+                    className="text-3xl font-bold text-white text-center m-4 select-none"
                 >
-                    <h4
-                        className="text-xl font-bold text-center select-none"
-                    >
-                        swaps
-                    </h4>
+                    cell &lt;{data.value}&gt;
+                </h2>
 
-                    <ul
-                        className="flex flex-col items-center justify-center text-white"
-                    >
-                        {swaps.map((swap, i) => {
-                            const cell = distribution.find((cell) => cell.id === swap);
-                            if (!cell) {
-                                return;
-                            }
+                <h3
+                    className="text-xl font-bold text-white text-center m-4 select-none"
+                >
+                    &lsquo;{data.algotype}&rsquo; algotype · &lsquo;{data.swap || 'proactive'}&rsquo; swap
+                </h3>
 
-                            return (
-                                <li
-                                    key={i}
-                                    className="text-lg font-bold text-center m-2 select-none"
-                                >
-                                    swapped with {cell.value}
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            )}
+                {swaps.length > 0 && (
+                    <div
+                        className="flex flex-col text-white m-4"
+                    >
+                        <h4
+                            className="text-xl font-bold text-center select-none"
+                        >
+                            swaps
+                        </h4>
+
+                        <ul
+                            className="flex flex-col text-white"
+                        >
+                            {swaps.map((swap, i) => {
+                                const cell = distribution.find((cell) => cell.id === swap);
+                                if (!cell) {
+                                    return;
+                                }
+
+                                return (
+                                    <li
+                                        key={i}
+                                        className="text-lg font-bold text-center m-2 select-none"
+                                    >
+                                        swapped with {cell.value}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
