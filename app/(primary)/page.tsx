ec55e2f7a3b174cec65d3f0c5f511f64256cd7fe
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-05-29 10:41:51 +0300

    dev: app

--- app/(primary)/page.tsx
+++ app/(primary)/page.tsx
@@ -57,28 +57,32 @@ export default function Home() {
                     className="text-xs my-2"
                 >
                     Piatra Institute of Arts & Technologies Recursively Applied
-                    <a
-                        href="mailto:contact@piatra.institute"
-                        tabIndex={-1}
+                    <span
+                        style={{
+                            marginLeft: '0.1rem',
+                            padding: '0.1rem',
+                        }}
                     >
-                        <svg
-                            width="14px" height="14px"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                            tabIndex={1}
+                        <a
+                            href="mailto:contact@piatra.institute"
                             className={focusStyle}
-                            style={{
-                                display: 'inline',
-                                marginLeft: '0.4rem',
-                                padding: '0.1rem',
-                            }}
                         >
-                            <path
-                                d="M22 2L2 8.66667L11.5833 12.4167M22 2L15.3333 22L11.5833 12.4167M22 2L11.5833 12.4167"
-                                stroke="#f7fee7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                            />
-                        </svg>
-                    </a>
+                            <svg
+                                width="14px" height="14px"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                                style={{
+                                    display: 'inline',
+                                    padding: '0.1rem',
+                                }}
+                            >
+                                <path
+                                    d="M22 2L2 8.66667L11.5833 12.4167M22 2L15.3333 22L11.5833 12.4167M22 2L11.5833 12.4167"
+                                    stroke="#f7fee7" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                />
+                            </svg>
+                        </a>
+                    </span>
                 </h2>
 
                 <div
