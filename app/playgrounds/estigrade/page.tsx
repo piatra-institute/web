b0da1669ee1fefd38561cfa28a799b3d9b142555
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-09-25 19:55:17 +0300

    dev: playgrounds

--- app/playgrounds/estigrade/page.tsx
+++ app/playgrounds/estigrade/page.tsx
@@ -7,6 +7,19 @@ import EstigradePlayground from './playground';
 export const metadata: Metadata = {
     title: 'estigrade · playgrounds',
     description: 'enhances grades when students accurately estimate their exam scores',
+
+    openGraph: {
+        type: 'website',
+        url: 'https://piatra.institute',
+        title: 'estigrade · playgrounds · piatra.institute',
+        description: 'enhances grades when students accurately estimate their exam scores',
+        siteName: 'piatra.institute',
+        images: [
+            {
+                url: 'https://piatra.institute/og.png',
+            },
+        ],
+    },
 };
 
 export default function Estigrade() {
--- app/playgrounds/self-sorted-arrays/page.tsx
+++ app/playgrounds/self-sorted-arrays/page.tsx
@@ -7,6 +7,19 @@ import SelfSortedArraysPlayground from './playground';
 export const metadata: Metadata = {
     title: 'self-sorted arrays · playgrounds',
     description: 'arrays sorted bottom-up with cell-like numbers',
+
+    openGraph: {
+        type: 'website',
+        url: 'https://piatra.institute',
+        title: 'self-sorted arrays · playgrounds · piatra.institute',
+        description: 'arrays sorted bottom-up with cell-like numbers',
+        siteName: 'piatra.institute',
+        images: [
+            {
+                url: 'https://piatra.institute/og.png',
+            },
+        ],
+    },
 };
 
 export default function SelfSortedArrays() {
