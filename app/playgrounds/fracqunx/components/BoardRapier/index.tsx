9f02c4df5bad03df6e68acb8046ab690dad28ab1
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2025-02-11 12:40:00 +0200

    dev: playgrounds | fracqunx rapier

--- app/playgrounds/fracqunx/components/BoardRapier/index.tsx
+++ app/playgrounds/fracqunx/components/BoardRapier/index.tsx
@@ -12,6 +12,21 @@ import {
     OrthographicCamera, Box, OrbitControls,
 } from '@react-three/drei';
 
+import * as THREE from 'three';
+
+
+
+const wallColor = '#FFD700';
+const beadColor = '#50C878';
+const pegColor = '#C0C0C0';
+
+const beadYStart = 4.5
+
+
+export interface Bead {
+    id: number;
+    position: number[];
+}
 
 
 function Container() {
@@ -23,14 +38,14 @@ function Container() {
             {/* Bottom */}
             <RigidBody type="fixed">
                 <Box args={[4, 0.2, thickness * 3]} position={[0, -2.3, 0]}>
-                    <meshStandardMaterial color="red" />
+                    <meshStandardMaterial color={wallColor} />
                 </Box>
             </RigidBody>
 
             {/* Top */}
             <RigidBody type="fixed">
                 <Box args={[4, 0.2, thickness * 3]} position={[0, 5.9, 0]}>
-                    <meshStandardMaterial color="red" />
+                    <meshStandardMaterial color={wallColor} />
                 </Box>
             </RigidBody>
 
@@ -40,7 +55,7 @@ function Container() {
                     args={[2, 0.2, thickness]} position={[-1.05, 4.8, 0]}
                     rotation={[0, 0, -Math.PI / 7]}
                 >
-                    <meshStandardMaterial color="red" />
+                    <meshStandardMaterial color={wallColor} />
                 </Box>
             </RigidBody>
 
@@ -50,7 +65,7 @@ function Container() {
                     args={[2, 0.2, thickness]} position={[1.05, 4.8, 0]}
                     rotation={[0, 0, Math.PI / 7]}
                 >
-                    <meshStandardMaterial color="red" />
+                    <meshStandardMaterial color={wallColor} />
                 </Box>
             </RigidBody>
 
@@ -58,7 +73,16 @@ function Container() {
             <RigidBody type="fixed">
                 {/* Left */}
                 <Box args={[0.2, 8.2, thickness]} position={[-1.9, 1.7, 0]}>
-                    <meshStandardMaterial color="red" transparent opacity={1} />
+                    <meshStandardMaterial
+                        color={wallColor}
+                    />
+                </Box>
+
+                {/* Right */}
+                <Box args={[0.2, 8.2, thickness]} position={[1.9, 1.7, 0]}>
+                    <meshStandardMaterial
+                        color={wallColor}
+                    />
                 </Box>
 
                 {Array.from({ length: 18 }).map((_, i) => (
@@ -67,25 +91,25 @@ function Container() {
                         args={[0.02, 2, thickness]} position={[-1.7 + 0.2 * i, -1.2, 0]}
                     >
                         <meshStandardMaterial
-                            color="red" transparent opacity={1}
-                            emissive="red"
+                            color={wallColor}
+                            emissive={wallColor}
+                            transparent={false}
                         />
                     </Box>
                 ))}
 
-                {/* Right */}
-                <Box args={[0.2, 8.2, thickness]} position={[1.96, 1.7, 0]}>
-                    <meshStandardMaterial color="red" transparent opacity={1} />
-                </Box>
-
                 {/* Back */}
                 <Box args={[4, 8.2, 0.2]} position={[0, 1.7, -thickness - 0.02]}>
-                    <meshStandardMaterial color="red" transparent opacity={opacity} />
+                    <meshStandardMaterial color={wallColor} transparent opacity={opacity}
+                        side={THREE.DoubleSide}
+                    />
                 </Box>
 
                 {/* Front */}
                 <Box args={[4, 8.2, 0.2]} position={[0, 1.7, thickness + 0.02]}>
-                    <meshStandardMaterial color="red" transparent opacity={opacity} />
+                    <meshStandardMaterial color={wallColor} transparent opacity={opacity}
+                        side={THREE.DoubleSide}
+                    />
                 </Box>
             </RigidBody>
         </>
@@ -104,33 +128,101 @@ function Bead({ position, radius = 0.04 }: any) {
             linearDamping={0.2}
             angularDamping={0.2}
             mass={0.2}
-            onSleep={() => console.log('sleep')}
+            // onSleep={() => console.log('sleep')}
         >
             <mesh>
                 <sphereGeometry args={[radius]} />
-                <meshStandardMaterial color="red" />
+                <meshStandardMaterial
+                    color={beadColor}
+                    emissive={beadColor}
+                />
             </mesh>
         </RigidBody>
     )
 }
 
 
-
-function Board() {
+function Scene({
+    beads,
+} : {
+    beads: Bead[],
+}) {
     const pegsYStart = 6
-    const beadYStart = 4.5
 
     const width = 10
     const height = 30
     const pegRadius = 0.08
     const pegSpacing = 0.3
 
-    const [beads, setBeads] = useState<any[]>([
-        // ...Array.from({ length: 1000 }).map(() => ({
-        //     id: Date.now(),
-        //     position: [(Math.random() - 0.5) * 2, 6, 0]
-        // }))
-    ])
+    const pegs = []
+    for (let row = 0; row < 14; row++) {
+        // const numPegsInRow = 14 - Math.floor(row / 2)
+        const numPegsInRow = 11
+        for (let col = 0; col < numPegsInRow; col++) {
+            const offset = row % 2 === 0 ? 0 : 0.15
+            const x = -0.05 + (col - (numPegsInRow - 1) / 2) * pegSpacing + offset
+            const y = pegsYStart + -row * pegSpacing - 2
+            pegs.push([x, y])
+        }
+    }
+
+    return (
+        <>
+            <ambientLight intensity={1} />
+            <directionalLight position={[-10, 10, 0]} intensity={1} />
+
+            {/* <OrthographicCamera
+                makeDefault
+                position={[0, 0, 10]}
+                zoom={80}
+            /> */}
+            <OrthographicCamera
+                makeDefault
+                position={[0, 0, 10]}
+                zoom={80}
+                near={0.1}
+                far={1000}
+            />
+            <OrbitControls />
+
+            <RigidBody type="fixed">
+                <CuboidCollider args={[0.5, height, 1]} position={[-width / 2, 0, 0]} />
+                <CuboidCollider args={[0.5, height, 1]} position={[width / 2, 0, 0]} />
+                <CuboidCollider args={[width, 0.5, 1]} position={[0, -height / 2, 0]} />
+            </RigidBody>
+
+            {pegs.map(([x, y], i) => (
+                <RigidBody
+                    key={i}
+                    type="fixed"
+                    position={[x, y, 0]}
+                    // restitution={0.7}
+                    colliders="ball"
+                >
+                    <mesh>
+                        <sphereGeometry args={[pegRadius]} />
+                        <meshStandardMaterial
+                            color={pegColor}
+                        />
+                    </mesh>
+                </RigidBody>
+            ))}
+
+            <Container />
+
+            {beads.map((bead, index) => (
+                <Bead key={index + bead.id} position={bead.position} />
+            ))}
+        </>
+    );
+}
+
+
+function Board() {
+    const [beads, setBeads] = useState<Bead[]>([]);
+    const [isRunning, setIsRunning] = useState(false);
+    const [areaOfEffect, setAreaOfEffect] = useState(false);
+    const [morphodynamics, setMorphodynamics] = useState(false);
 
 
     // const spawnBead = () => {
@@ -178,16 +270,8 @@ function Board() {
     };
 
 
-    const pegs = []
-    for (let row = 0; row < 14; row++) {
-        // const numPegsInRow = 14 - Math.floor(row / 2)
-        const numPegsInRow = 11
-        for (let col = 0; col < numPegsInRow; col++) {
-            const offset = row % 2 === 0 ? 0 : 0.15
-            const x = -0.05 + (col - (numPegsInRow - 1) / 2) * pegSpacing + offset
-            const y = pegsYStart + -row * pegSpacing - 2
-            pegs.push([x, y])
-        }
+    const reset = () => {
+        setBeads([]);
     }
 
 
@@ -202,54 +286,66 @@ function Board() {
 
     return (
         <div className="h-dvh w-full">
-            <button
-                onClick={spawnBead}
-                className="absolute z-50 bottom-4 left-4 px-4 py-2 bg-lime-50 text-black"
-            >
-                Drop Beads
-            </button>
-
             <Canvas>
-                <OrthographicCamera
-                    makeDefault
-                    position={[0, 0, 10]}
-                    zoom={80}
-                />
-                <OrbitControls />
-
-                <ambientLight intensity={1} />
-                <directionalLight position={[-10, 10, 0]} intensity={1} />
                 <Physics gravity={[0, -9.81, 0]}>
-                    <RigidBody type="fixed">
-                        <CuboidCollider args={[0.5, height, 1]} position={[-width / 2, 0, 0]} />
-                        <CuboidCollider args={[0.5, height, 1]} position={[width / 2, 0, 0]} />
-                        <CuboidCollider args={[width, 0.5, 1]} position={[0, -height / 2, 0]} />
-                    </RigidBody>
-
-                    {pegs.map(([x, y], i) => (
-                        <RigidBody
-                            key={i}
-                            type="fixed"
-                            position={[x, y, 0]}
-                            // restitution={0.7}
-                            colliders="ball"
-                        >
-                            <mesh>
-                                <sphereGeometry args={[pegRadius]} />
-                                <meshStandardMaterial color="gray" />
-                            </mesh>
-                        </RigidBody>
-                    ))}
-
-                    <Container />
-
-                    {beads.map((bead, index) => (
-                        <Bead key={index + bead.id} position={bead.position} />
-                    ))}
+                    <Scene
+                        beads={beads}
+                    />
                 </Physics>
             </Canvas>
+
+            <div
+                className="absolute bottom-8 left-0 right-0"
+            >
+                <div
+                    className="flex gap-4 m-4 items-center justify-center"
+                >
+                    <button
+                        onClick={() => reset()}
+                        className="px-4 py-2 bg-lime-50 min-w-[180px] text-black hover:bg-lime-200 transition-colors"
+                    >
+                        Reset
+                    </button>
+                    <button
+                        onClick={() => setIsRunning(!isRunning)}
+                        className={
+                            `px-4 py-2 min-w-[180px] text-black hover:bg-lime-200 transition-colors ${isRunning ? 'bg-lime-50' : 'bg-lime-200'}`
+                        }
+                    >
+                        {isRunning ? 'Pause' : 'Resume'}
+                    </button>
+                    <button
+                        onClick={spawnBead}
+                        className="px-4 py-2 bg-lime-50 min-w-[180px] text-black hover:bg-lime-200 transition-colors"
+                    >
+                        Add Bead
+                    </button>
+                </div>
+
+                <div
+                    className="flex gap-4 m-4 items-center justify-center"
+                >
+                    <button
+                        onClick={() => setAreaOfEffect(!areaOfEffect)}
+                        className={
+                            `px-4 py-2 min-w-[180px] text-black hover:bg-lime-200 transition-colors ${areaOfEffect ? 'bg-lime-200' : 'bg-lime-50'}`
+                        }
+                    >
+                        Area of Effect
+                    </button>
+                    <button
+                        onClick={() => setMorphodynamics(!morphodynamics)}
+                        className={
+                            `px-4 py-2 min-w-[180px] text-black hover:bg-lime-200 transition-colors ${morphodynamics ? 'bg-lime-200' : 'bg-lime-50'}`
+                        }
+                    >
+                        Morphodynamics
+                    </button>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
+
 
 export default Board;
