60eadd08dc99f41f05897ac66140ec0de6e39f9f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2025-07-03 08:50:51 +0300

    dev: playgrounds

--- app/playgrounds/page.tsx
+++ app/playgrounds/page.tsx
@@ -21,116 +21,139 @@ const playgrounds = [
         name: 'self-sorted arrays',
         link: '/playgrounds/self-sorted-arrays',
         description: 'cell-based sorting algorithms',
+        date: 'February 2024',
     },
     {
         name: 'estigrade',
         link: '/playgrounds/estigrade',
-        description: 'grade estimation'
+        description: 'grade estimation',
+        date: 'September 2024',
     },
     {
         name: 'coasellular morphogenesis',
         link: '/playgrounds/coasellular-morphogenesis',
         description: 'Coase theorem applied to cellular development and transaction costs',
+        date: 'November 2024',
     },
     {
         name: 'fracqunx',
         link: '/playgrounds/fracqunx',
         description: 'fractional quincunx',
+        date: 'February 2025',
     },
     {
         name: 'Halley window',
         link: '/playgrounds/halley-window',
         description: 'Halley\'s method fractal patterns',
-    },
-    {
-        name: 'Raupian morphospace',
-        link: '/playgrounds/raupian-morphospace',
-        description: 'coiling models',
+        date: 'February 2025',
     },
     // {
     //     name: 'eyevolution',
     //     link: '/playgrounds/eyevolution',
     //     description: '',
+    //     date: 'March 2024',
     // },
     // {
     //     name: 'pacemaker-accumulator',
     //     link: '/playgrounds/pacemaker-accumulator',
     //     description: '',
+    //     date: 'March 2024',
     // },
     // {
     //     name: 'cpt-variance',
     //     link: '/playgrounds/cpt-variance',
     //     description: '',
+    //     date: 'March 2024',
     // },
     // {
     //     name: 'refractive computation',
     //     link: '/playgrounds/refractive-computation',
     //     description: '',
+    //     date: 'March 2024',
     // },
     // {
     //     name: 'metamaterials',
     //     link: '/playgrounds/metamaterials',
     //     description: '',
+    //     date: 'March 2024',
     // },
     // {
     //     name: 'lifesong',
     //     link: '/playgrounds/lifesong',
     //     description: '',
+    //     date: 'March 2024',
     // },
     // {
     //     name: 'vote no',
     //     link: '/playgrounds/vote-no',
     //     description: '',
+    //     date: 'March 2024',
     // },
     // {
     //     name: 'everything... relevant',
     //     link: '/playgrounds/everything-relevant',
     //     description: '',
+    //     date: 'March 2024',
     // },
+    {
+        name: 'Raupian morphospace',
+        link: '/playgrounds/raupian-morphospace',
+        description: 'coiling models',
+        date: 'April 2025',
+    },
     {
         name: 'Hsp90 canalization',
         link: '/playgrounds/hsp90-canalization',
         description: 'chaperone-driven canalization of latent phenotypic variation',
+        date: 'April 2025',
     },
     {
         name: 'Lefebvrian conscience',
         link: '/playgrounds/lefebvrian-conscience',
         description: 'algebra of conscience',
+        date: 'May 2025',
     },
     {
         name: 'bifurcation speciation',
         link: '/playgrounds/bifurcation-speciation',
         description: 'sympatric and allopatric speciation through bifurcation',
+        date: 'May 2025',
     },
     {
         name: 'stochastic justice',
         link: '/playgrounds/stochastic-justice',
         description: 'fairness through randomness in corrupt systems',
+        date: 'June 2025',
     },
     {
         name: 'trauma-eustress dynamics',
         link: '/playgrounds/trauma-eustress-dynamics',
         description: 'constriction and expansion influence on post-trauma trajectories',
+        date: 'June 2025',
     },
     {
         name: 'open source sustainability',
         link: '/playgrounds/open-source-sustainability',
         description: 'pressure dynamics in open source license transitions',
+        date: 'June 2025',
     },
     {
         name: 'resentment against desire',
         link: '/playgrounds/resentment-against-desire',
         description: 'the psychology of fairness in the ultimatum game',
+        date: 'June 2025',
     },
     {
         name: 'debt singularity',
         link: '/playgrounds/debt-singularity',
         description: 'phase transitions of debt through economic conditions',
+        date: 'June 2025',
     },
     // {
     //     name: 'entropy café',
     //     link: '/playgrounds/entropy-cafe',
     //     description: 'entropy and complexity through coffee mixing metaphor',
+    //     date: 'July 2025',
     // },
 ];
 
@@ -154,6 +177,7 @@ export default function Playgrounds() {
                         name,
                         link,
                         description,
+                        date,
                     } = playground;
 
                     return (
@@ -166,7 +190,7 @@ export default function Playgrounds() {
                             <div
                                 className={linkAnchorStyle}
                             >
-                                {name}
+                                {name} · {date}
                             </div>
 
                             {description && (
