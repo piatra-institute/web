6ecf1263c9ffdfd9f00bba6a94c0cbd60a72d673
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2025-07-24 17:28:51 +0300

    dev: platforms

--- app/platforms/page.tsx
+++ app/platforms/page.tsx
@@ -48,6 +48,11 @@ const platforms = [
     //     link: 'https://sonocracy.com',
     // },
     {
+        name: 'catala . ro',
+        description: 'Romanian implementation of the Catala language for programmatic law',
+        link: 'https://catala.ro',
+    },
+    {
         name: 'xfactura . ro',
         description: 'einvoicing and general accounting following the Romanian/EU legislation',
         link: 'https://xfactura.ro',
